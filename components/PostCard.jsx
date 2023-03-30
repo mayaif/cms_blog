46002c86cf7b1565c88eae180097aadc0c4f50ed
mayaif <margareta.ifrim@outlook.com>
2023-03-30 15:19:37 +0100

    refactor Postcard UI

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -18,7 +18,7 @@ function PostCard({post}) {
           src={post.featuredImage.url}
           alt={post.title}
           
-          className='object-top absolute h-auto lg:h-80 w-full object-cover shadow-lg rounded-lg lg:rounded-lg '
+          className='object-top absolute h-80 w-full object-cover  shadow-lg rounded-lg lg:rounded-lg'
         />
       </div>
       <h1 className='transition duration-700 text-center text-slate-800 mb-8 cursor-pointer hover:text-pink-600 text-3xl font-semibold'>
