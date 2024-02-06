28ca7caa8dc859b4c1905511637d54555080670b
mayaif <margareta.ifrim@outlook.com>
2024-02-06 17:35:14 +0000

    remove comment

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,74 +1,80 @@
-import React from 'react'
-import {getPosts, getPostDetails} from '../../services'
-import {PostDetail, Categories, PostWidget, Author, Comments, CommentsForm, Loader} from '../../components'
+import React from "react";
+import { getPosts, getPostDetails } from "../../services";
+import {
+  PostDetail,
+  Categories,
+  PostWidget,
+  Author,
+  Comments,
+  CommentsForm,
+  Loader,
+} from "../../components";
 
-import { useRouter } from 'next/router'
-import Head from 'next/head';
+import { useRouter } from "next/router";
+import Head from "next/head";
 
+function PostDetails({ post }) {
+  const router = useRouter();
 
-
-function PostDetails({post}) {
-  const router = useRouter()
-
-  if(router.isFallback) {
-    return <Loader />
+  if (router.isFallback) {
+    return <Loader />;
   }
 
   return (
-    <div className='container mx-auto px-10 mb-8 text-slate-800'>
+    <div className="container mx-auto px-10 mb-8 text-slate-800">
       <Head>
         <title>{post.title}</title>
         <meta name="description" content={post.excerpt} />
         <meta name="keywords" content={post.tags} />
         <meta property="og:title" content={post.title} />
         <meta property="og:description" content={post.excerpt} />
-        <meta property="og:url" content={`https://codemind.tech/${post.slug}`} />
+        <meta
+          property="og:url"
+          content={`https://codemind.tech/${post.slug}`}
+        />
         <meta name="twitter:title" content={post.title} />
         <meta name="twitter:description" content={post.excerpt} />
         <meta name="twitter:card" content="summary_large_image" />
-    </Head>
-      <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
-        <div className='col-span-1 lg:col-span-8'>
+      </Head>
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        <div className="col-span-1 lg:col-span-8">
           <PostDetail post={post} />
-          <Author author={post.author}/>
-          {/* <AdjacentPosts slug={post.slug} createdAt={post.createdAt} /> */}
-          <CommentsForm slug={post.slug}/>
-          <Comments slug={post.slug}/>
+          <Author author={post.author} />
+          <CommentsForm slug={post.slug} />
+          <Comments slug={post.slug} />
         </div>
-        <div className='col-span-1 lg:col-span-4'>
-          <div className='relative lg:sticky top-8'>
-            <PostWidget 
+        <div className="col-span-1 lg:col-span-4">
+          <div className="relative lg:sticky top-8">
+            <PostWidget
               slug={post.slug}
-              categories={post.categories.map(category => (
-                category.slug
-              ))}
+              categories={post.categories.map((category) => category.slug)}
             />
             <Categories />
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default PostDetails
+export default PostDetails;
 
 // Fetch data at build time
 // the 'params' will be the slug
 export async function getStaticProps({ params }) {
-  const data = await getPostDetails(params.slug)
+  const data = await getPostDetails(params.slug);
   return {
-    props: { post: data }
-  }
+    props: { post: data },
+  };
 }
 
 // Dynamic routes to pre-render pages based on data.
 // The HTML is generated at build time and will be reused on each request.
 export async function getStaticPaths() {
-  const posts = await getPosts()
+  const posts = await getPosts();
 
   return {
-    paths: posts.map(({node: {slug}}) => ({params: {slug}})),
-    fallback: true
-  }
+    paths: posts.map(({ node: { slug } }) => ({ params: { slug } })),
+    fallback: true,
+  };
 }
